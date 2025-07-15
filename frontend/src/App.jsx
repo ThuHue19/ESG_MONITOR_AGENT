6b7c31170962cf7c4de30a07c6b62a355f2b6c13
baongocmai <maithibaongoc_t67@hus.edu.vn>
2025-07-15 17:59:50 +0700

    fix: add postinstall to fix react-scripts permission error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import ArticleDetail from './components/ArticleDetail';
 import ReactMarkdown from 'react-markdown';
 import EsgInfo from './components/EsgInfo';
 
-const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+const API_BASE = "https://esg-monitor-agent.onrender.com" || 'http://localhost:8000';
 
 const companyToTicker = {
   Tesla: 'TSLA',
